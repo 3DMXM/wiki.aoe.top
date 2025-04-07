03718dea2345b10c0ed10944b5a45ae191c7df91
小莫 <xiaomo1433675239@outlook.com>
2025-04-07 15:57:20 +0800

    添加评论功能

---
+++ docs/.vitepress/theme/components/GiscusComment.vue
@@ -0,0 +1,20 @@
+<template>
+    <div style="margin-top: 24px">
+        <Giscus id="comments" repo="3DMXM/wiki.aoe.top" repoid="R_kgDOJcmFng" category="General"
+            categoryid="DIC_kwDOJcmFns4Co2n1" mapping="pathname" term="Welcome to giscus!" reactionsenabled="1"
+            lang="zh-CN" emitmetadata="0" inputposition="top" loading="lazy" :theme="isDark ? 'dark' : 'light'"
+            :key="route.path">
+        </Giscus>
+    </div>
+</template>
+
+<script setup>
+import Giscus from '@giscus/vue';
+import { useRoute, useData } from 'vitepress';
+
+
+
+
+const route = useRoute();
+const { isDark } = useData();
+</script>
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,14 +1,13 @@
-// .vitepress/theme/index.js
-import { h } from 'vue'
-import DefaultTheme from 'vitepress/theme'
-import Documate from '@documate/vue'
-import '@documate/vue/dist/style.css'
+import { h } from 'vue';
+import Theme from 'vitepress/theme';
+import GiscusComment from './components/GiscusComment.vue';
+
 
 export default {
-    ...DefaultTheme,
-    // Layout: h(DefaultTheme.Layout, null, {
-    //     'nav-bar-content-before': () => h(Documate, {
-    //         endpoint: 'https://es1fryngs7.us.aircode.run/ask',
-    //     }),
-    // }),
-}
+    ...Theme,
+    Layout() {
+        return h(Theme.Layout, null, {
+            'doc-after': () => h(GiscusComment),
+        });
+    },
+};
