e10ddc3b015f37919e5004d2bb711b7e4eb09742
小莫 <xiaomo1433675239@outlook.com>
2025-04-08 14:06:23 +0800

    添加图片放大功能

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -2,6 +2,9 @@ import { h } from 'vue';
 import Theme from 'vitepress/theme';
 import GiscusComment from './components/GiscusComment.vue';
 
+import imageViewer from 'vitepress-plugin-image-viewer';
+import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue';
+import { useRoute } from 'vitepress';
 
 export default {
     ...Theme,
@@ -10,4 +13,17 @@ export default {
             'doc-after': () => h(GiscusComment),
         });
     },
+    enhanceApp(ctx) {
+        Theme.enhanceApp(ctx);
+        // Register global components, if you don't want to use it, you don't need to add it
+        ctx.app.component('vImageViewer', vImageViewer);
+        // ...
+    },
+    setup() {
+        // Get route
+        const route = useRoute();
+        // Using
+        imageViewer(route);
+    }
+
 };
