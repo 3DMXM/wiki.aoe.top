a9b06249f3455342df1b39ac1912de0a69460fe1
小莫 <xm@aoe.top>
2025-06-12 15:29:26 +0800

    移除广告

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -89,18 +89,6 @@ export default withMermaid({
     head: [
         [
             'script',
-            {
-                async: "true",
-                src: 'https://www.googletagmanager.com/gtag/js?id=G-L04H04RSS7',
-            },
-        ],
-        [
-            'script',
-            {},
-            "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-L04H04RSS7');",
-        ],
-        [
-            'script',
             {},
             `var _hmt = _hmt || [];
             (function() {
